d75067f4a6ff6ed7051740df52def99d4aa2b0d1
Sabeer Sulaiman <sabeersulaimanpv@gmail.com>
2015-11-23 22:10:32 +0530

    Added basic ListView and hardcoded adapter data

--- app/src/main/java/gdg/mesce/sabeersulaiman/remindme/RemaindersActivity.java
+++ app/src/main/java/gdg/mesce/sabeersulaiman/remindme/RemaindersActivity.java
@@ -8,24 +8,30 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class RemaindersActivity extends AppCompatActivity {
+
+    private ListView mListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_remainders);
+        mListView = (ListView) findViewById(R.id.RemainderListView);
+
+        ArrayAdapter<String> stringArrayAdapter = new ArrayAdapter<String>(
+                this,
+                R.layout.remainder_list_item,
+                R.id.rowText,
+                new String[] {"First Record", "Second Record", "Third Record", "Final Record"}
+        );
+
+        mListView.setAdapter(stringArrayAdapter);
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
     @Override
